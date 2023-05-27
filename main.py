dceafbf68f809a8565ac31b070a2a81b2b4064fd
NadavC3 <nadav3395@gmail.com>
2023-05-27 12:42:04 +0300

    added ECDH part

--- main.py
+++ main.py
@@ -6,13 +6,40 @@ Created on Sat May  6 12:11:58 2023
 """
 
 import ARIA_FUNC as ARIA
+import diffie_hellman as ECDH
 
 def main():
+    
+
+    
+    #ECDH:
+    print("\n\n\n")
+    F = ECDH.FiniteField(3851, 1)
+    # Totally insecure curve: y^2 = x^3 + 324x + 1287
+    curve = ECDH.EllipticCurve(a=F(324), b=F(1287))
+    # order is 1964
+    basePoint = ECDH.Point(curve, F(920), F(303))
+
+    aliceSecretKey = ECDH.generateSecretKey(8)
+    bobSecretKey = ECDH.generateSecretKey(8)
+
+    print('Secret keys are %d, %d' % (aliceSecretKey, bobSecretKey))
+
+    alicePublicKey = ECDH.sendDH(aliceSecretKey, basePoint, lambda x:x)
+    bobPublicKey = ECDH.sendDH(bobSecretKey, basePoint, lambda x:x)
+
+    sharedSecret1 = ECDH.receiveDH(bobSecretKey, lambda: alicePublicKey)
+    sharedSecret2 = ECDH.receiveDH(aliceSecretKey, lambda: bobPublicKey)
+    print('Shared secret is %s == %s' % (sharedSecret1, sharedSecret2))
+    print('extracing x-coordinate to get an integer shared secret: %d' % (sharedSecret1.x.n))
+    
+    
+    
     my_string = 'monkey'
     plain = int(my_string.encode('utf-8').hex(), 16)
-
-    key = 0x0123456789abcdef0123456789abcdef
+    key = sharedSecret1.x.n
     bits = 256
+    
 
     cipher = ARIA.ARIA_encryption(plain, key, bits)
     decrypted = ARIA.ARIA_decryption(cipher, key, bits)
@@ -20,11 +47,15 @@ def main():
     original = bytes.fromhex(decrypted_str).decode('utf-8')
 
     # Print the results
-    print("Plain text: ",my_string)
+    print("\n\n\nPlain text: ",my_string)
     print("Key: {0:0{1}x}".format(key, bits//4))
     print("Cipher text: {0:032x}".format(cipher))
     print("Decrypted text: {0:032x}".format(decrypted))
     print("original text: ",original)
+    
+    
+
+    
 
 
 # Press the green button in the gutter to run the script.
